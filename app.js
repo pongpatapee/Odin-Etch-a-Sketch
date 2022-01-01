38146ea9fe162955aacb65007b9ce51eabef7d63
pongpatapee <pongpatapee1021@gmail.com>
2022-01-01 15:12:05 -0800

    got buttons and mode working

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const defaultCanvSize = 16;
 const defaultColor = "#333333";
 const defaultMode = "color";
-
+const modes = ["color", "rainbow", "eraser"];
 // let CANV_SIZE = document.getElementById('canv-size').value;
 let CANV_SIZE = defaultCanvSize;
 let currentColor = defaultColor;
@@ -15,21 +15,42 @@ const clearBtn = document.getElementById('clear');
 const canvSizeInput = document.getElementById('canv-size');
 const canvSizeDisplay = document.getElementById('canv-size-display');
 let cells;
+let currentActiveBtn = colorModeBtn;
+currentActiveBtn.classList.add('active');
 
-function changeMode(mode){
+function getMode(modeid) {
+    let mode = modeid.slice(0, modeid.length - 5);
+    return mode;
+}
 
+function changeMode(modeid){
+    let mode = getMode(modeid);
+    currentMode = mode;
+    currentActiveBtn.classList.remove('active');
+    currentActiveBtn = document.getElementById(modeid);
+    currentActiveBtn.classList.add('active');
 }
 
 function changeColor(newColor){
     currentColor = newColor;
 }
 
-function changeSize(size) {
+function generateRandomColor() {
+    let letters = '0123456789ABCDEF'
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
 
+    return color;
+}
+
+function updateSizeDisplay(size) {
+    canvSizeDisplay.innerText = `${size} x ${size}`;
 }
 
 function updateCanvSize(size){
-    canvSizeDisplay.innerText = `${size} x ${size}`;
+    updateSizeDisplay(size);
     CANV_SIZE = size;
     renderCanvas();
 }
@@ -51,12 +72,31 @@ function renderCanvas() {
     }
     cells = document.querySelectorAll('.canvas div div');
     cells.forEach(cell => cell.addEventListener('mouseover', () => {
-        cell.style.background = currentColor;
-        // console.log(cell.style);
+        if(currentMode === "color"){
+            cell.style.background = currentColor;
+        }
+        else if (currentMode === "rainbow") {
+            cell.style.background = generateRandomColor();
+        }
+        else if (currentMode === "eraser") {
+            cell.style.background = "none";
+        }
     }));
 }
 
 
 renderCanvas();
 
+colorModeBtn.addEventListener('click', (e)=>{
+    changeMode(e.target.id);
+});
+rainbowModeBtn.addEventListener('click', (e)=>{
+    changeMode(e.target.id);
+});
+eraserModeBtn.addEventListener('click', (e)=>{
+    changeMode(e.target.id);
+});
 clearBtn.addEventListener('click', clearCanv);
+canvSizeInput.addEventListener('mousemove', (e)=>{
+    updateSizeDisplay(e.target.value);
+});
